762fb952f8d699b972b79f0ad8d58eea20a9eaa9
gsambora <gsambora@graces-macbook-pro-3.local>
2018-11-29 15:16:02 -0500

    initial commit

--- src/main/java/edu/smith/cs/csc212/p5/P5Main.java
+++ src/main/java/edu/smith/cs/csc212/p5/P5Main.java
@@ -6,7 +6,7 @@ import java.awt.Graphics2D;
 import me.jjfoley.gfx.GFX;
 
 public class P5Main extends GFX {
-
+ 
   public void draw(Graphics2D g) {
     g.setColor(Color.red);
     g.fillRect(0,0,getWidth(), getHeight());
