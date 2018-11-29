d4c2c5c5184df751e7e7f24c82c625758d087c35
gsambora <gsambora@graces-macbook-pro-3.local>
2018-11-29 16:37:18 -0500

    Player is designed and moves back and forth!

---
+++ src/main/java/edu/smith/cs/csc212/p5/Player.java
@@ -0,0 +1,41 @@
+package edu.smith.cs.csc212.p5;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+
+import me.jjfoley.gfx.GFX;
+
+public class Player extends GFX {
+	public int x = 250;
+	public int y = 420;
+	
+	public Player() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void draw(Graphics2D g) {
+		// TODO When shooting from player, start at x+30, y-20
+		g.setColor(Color.white);
+		g.drawLine(x+30, y, x+30, y-20);
+		g.fillOval(x+25,y-20,10,10);
+		g.drawArc(x, y, 60, 60, 0, 180);
+		g.drawOval(x+15, y+10, 30, 35);
+		g.setColor(Color.green);
+		g.fillOval(x+15, y+10, 30, 35);
+		g.setColor(Color.black);
+		g.fillOval(x+21, y+18, 7, 7 );
+		g.fillOval(x+32, y+18, 7, 7 );
+		
+		g.setColor(Color.gray);
+		g.fillOval(x-17, y+27, 95, 30);
+		
+		g.setColor(Color.yellow);
+		g.fillOval(x-5, y+37, 10, 10);
+		g.fillOval(x+15, y+37, 10, 10);
+		g.fillOval(x+35, y+37, 10, 10);
+		g.fillOval(x+55, y+37, 10, 10);
+		
+	}
+
+}
--- src/main/java/edu/smith/cs/csc212/p5/World.java
+++ src/main/java/edu/smith/cs/csc212/p5/World.java
@@ -2,6 +2,7 @@ package edu.smith.cs.csc212.p5;
 
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.event.KeyEvent;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -9,11 +10,16 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
+import com.sun.glass.events.MouseEvent;
+
 import me.jjfoley.gfx.GFX;
 
 public class World extends GFX {
 	 
 	public String fileName = "src/main/Images/SpaceInvaders.png";
+	
+	Player Player = new Player();
+	
 	//ImagePanel class
 	public class ImagePanel extends JPanel {
 
@@ -44,9 +50,30 @@ public class World extends GFX {
 	}
 
 	@Override
+	public void update(double secondsSinceLastUpdate) {
+		boolean left = this.processKey(KeyEvent.VK_A) || this.processKey(KeyEvent.VK_LEFT);
+		boolean right = this.processKey(KeyEvent.VK_D) || this.processKey(KeyEvent.VK_RIGHT);
+		
+		if (( Player.x >= 8 ) && (left) ) {
+			Player.x -= 15;
+		} else if (( Player.x <= 412 ) && ( right ) ) {
+			Player.x += 15;
+		}
+		// TODO add mouse click here  
+		boolean shoot = this.processKey(KeyEvent.VK_SPACE) || this.processKey(KeyEvent.VK_UP) ;//|| (this.processKey(MouseEvent.CLICK) );//&& this.processKey(KeyEvent.KEY_RELEASED));
+		
+		if ( shoot ) {
+			System.out.println("shooting!!!!!");
+		}
+	}
+	
+	
+	@Override
 	public void draw(Graphics2D g) {
 		ImagePanel image = new ImagePanel(fileName);
 		image.paintComponent(g);
+		
+		Player.draw(g);
 		
 	}
 
